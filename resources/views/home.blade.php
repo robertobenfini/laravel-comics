9e69d14b620db566bcef5682217e8480c2d799f8
robertobenfini <roberto.benfini@gmail.com>
2023-07-21 16:56:01 +0200

    [ADD] card e button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,32 @@
 
     @include('partials.header')
 
+    <main>
+        <div>
+            <img class="jumbotron" src="{{ Vite::asset('resources/images/jumbotron.jpg') }}" alt="jumbo">
+        </div>
+        
+        <div class="bg-serie">
+            <div class="container">
+                <div class="row">
+                    @foreach($comics as $comic)
+                        <div class="col-12 col-md-6 col-lg-2 my-3">
+                            <div class="my-card">
+                                <img class="img-fluid img-comic" src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                                <div class="card-body">
+                                    <span><h4 class="title_h4">{{ $comic['series'] }}</h4></span>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="col-12 d-flex justify-content-center">
+                        <button class="btn-load">LOAD MORE</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
 </body>
 
 </html>
